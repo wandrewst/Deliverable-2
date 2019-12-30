36ac897cb5007a2026dc7d1997fd5261d936dcf9
Winnie R Davis <41178060+wandrewst@users.noreply.github.com>
2019-12-30 05:25:36 -0500

    Add files via upload

---
+++ Deliverable 2_/Deliverable 2_/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Deliverable_2_
+{
+    class Deliverable_2_
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Greetings! What is the message to code today? ");
+
+                string input = Console.ReadLine().ToUpper();
+
+                int checksum = 0;
+                string message = "";
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    int characterVal = (int)input[i];
+                    int alphaValue = (characterVal - 64);
+
+                    checksum += characterVal;
+                    message += alphaValue.ToString();
+
+                    if (i < input.Length - 1)
+                    {
+                        message += "-";
+                    }
+                }
+
+                Console.WriteLine("Your encoded message is " + message);
+                Console.WriteLine("Message checksum is " + checksum);
+
+                Console.ReadLine();
+            
+        } 
+    }
+}
